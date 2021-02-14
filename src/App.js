aa5bc57245cd043ab626d44736c876923a6f09ee
luci <lucia81@protonmail.com>
2021-02-14 06:06:49 -0300

    Universe

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'aframe-particle-system-component';
 import {Entity, Scene} from 'aframe-react';
 
 
+
 //require aframe physics system
 //require('aframe-physics-system');
 
@@ -16,19 +17,22 @@ class App extends React.Component {
 
   render(){
   return (
-<Scene physics = "debug=true" > 
+<Scene physics = "debug=true" >
 
-<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 2, z: -5}}/>
-<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 1, z: -5}}/>
-<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 0, z: -5}}/>
-<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 0, z: -4}}/>
+<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 1.8, z: -5}} scale={{x:0.9, y:0.1, z:1}}/>
+<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 0.2, z: -5}} scale={{x:0.9, y:0.1, z:1}}/>
+<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 1.5, z: -5}} scale={{x:0.6, y:0.6, z:0.6}}/>
+<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 1, z: -5}} scale={{x:0.6, y:0.6, z:0.6}}/>
+<Entity camera look-controls geometry={{primitive: 'box'}} material={{color: '#b1b4f4'}} position={{x: 4, y: 0.5, z: -5}}scale={{x:0.6, y:0.6, z:0.6}} />
 
 
 
-<Entity rigid-body={{x:2, y:3, z:3}}camera look-controls geometry={{primitive: 'sphere'}} material={{color: '#7cb275'}} position={{x: -5, y: 0, z: -4}} scale={{x: 2, y: 2, z: 2}}/>
+<Entity rigid-body={{x:2, y:3, z:3}}camera look-controls geometry={{primitive: 'sphere'}} material={{color: '#7c5275'}} position={{x: -5, y: -0.7, z: -4}} scale={{x: 0.2, y: 0.2, z: 0.2}}/>
+<Entity rigid-body={{x:2, y:3, z:3}}camera look-controls geometry={{primitive: 'sphere'}} material={{color: '#7c5275'}} position={{x: -3, y: -0.7, z: -5}} scale={{x: 0.2, y: 0.2, z: 0.2}}/>
+
 <Entity camera look-controls geometry={{primitive: 'cylinder'}} material={{color: '#7cb2a5'}} position={{x: 3, y: 7, z: 0}}/>
 
-<Entity primitive='a-sky' material={{color: '#68797f'}} />
+<a-sky id="image-360" radius="10" src="https://i.imgur.com/DrDomrj.jpg"></a-sky>
 
 <Entity particle-system={{preset: 'snow'}}/>
 
@@ -36,15 +40,55 @@ class App extends React.Component {
 
 <Entity particle-system={{preset: 'snow'}}/>
 <Entity geometry={{primitive: 'plane'}} material={{color: '#a8a8a8', roughness: 0.05}} rotation="-90 0 0" scale={{x: 105, y: 105, z:105}} position={{x: 5, y: -1, z: -5}}/>
-<Entity rotation={{x:0, y:-90, z:0 }} scale={{x: 20, y:20, z:20}} position={{x:1, y:5, z:1 }} text={{value: 'Developed by: \n Luci', color: '#000000'}}/>
+<Entity rotation={{x:90, y:-90, z:0 }} scale={{x: 20, y:20, z:20}} position={{x:1, y:8, z:6 }} text={{value: 'Developed by: \n Luci', color: '#fff'}}/>
+<Entity rotation={{x:90, y:-90, z:0 }} scale={{x: 15, y:15, z:15}} position={{x:-2, y:8, z:5 }} text={{value: 'Construindo mundos', color: '#fff'}}/>
 
 
 <Entity
     camera look-controls
-    geometry={{primitive: 'box', width: 2, height: 0.3}} position={{x:4,y:4,z:4}}
+    geometry={{primitive: 'box', width: 2, height: 0.1}} position={{x:4,y:4,z:4}}
     material={{color: '#b2fa4f', roughness: 0.05}}
-    scale={{x: 2, y: 2, z: 2}}
-    position={{x: 0, y:0, z: -5}}/>
+    scale={{x: 2, y: 1, z: 1}}
+    position={{x: 1, y:0.3, z: -3}}/>
+
+<Entity
+    camera look-controls
+    geometry={{primitive: 'box', width: 2, height: 0.1}} position={{x:4,y:4,z:4}}
+    material={{color: '#b2fa4f', roughness: 0.05}}
+    scale={{x: 2, y: 1, z: 1}}
+    position={{x: 1, y:0.3, z: 3}}/>
+
+<Entity
+    camera look-controls
+    geometry={{primitive: 'box', width: 2, height: 0.1}} position={{x:4,y:4,z:4}}
+    material={{color: '#b2fa4f', roughness: 0.05}}
+    scale={{x: 2, y: 1, z: 1}}
+    rotation={{x:0, y:90, z:0}}
+    position={{x: 4, y:0.3, z: 0}}/>
+
+
+<Entity
+    camera look-controls
+    geometry={{primitive: 'box', width: 2, height: 0.1}} position={{x:4,y:4,z:4}}
+    material={{color: '#b2fa4f', roughness: 0.05}}
+    scale={{x: 2, y: 1, z: 1}}
+    rotation={{x:0, y:90, z:0}}
+    position={{x: -1, y:0.3, z: 0}}/>
+
+<a-entity class="link"></a-entity>
+
+<a-entity class="link"
+  geometry="primitive: plane; height: 1; width: 1"
+  material="shader: flat; src: https://i.imgur.com/IDCLklw.jpg"></a-entity>
+
+<a-camera>
+  <a-cursor
+    id="cursor"
+    animation__click="property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click"
+    animation__clickreset="property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click"
+    animation__fusing="property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing"></a-cursor>
+</a-camera>
+
 </Scene>
 
 );
